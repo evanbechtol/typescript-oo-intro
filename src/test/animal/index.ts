32ad7e80a29fccb4e2e9404ba600472d5af9cb62
evan bechtol <evanbechtol@gmail.com>
2019-07-22 11:26:15 -0500

    Animal Test For Methods
    - Create tests to ensure methods are available

--- src/test/animal/index.ts
+++ src/test/animal/index.ts
@@ -11,52 +11,70 @@ before(() => {
 });
 
 describe("Animal Entity", () => {
-    it("Should create a valid Animal instance", () => {
-        expect(Buddy).to.be.an.instanceOf(Animal);
+    describe("Should have methods", () => {
+        it("Should have method ageIsValid", () => {
+            expect(Animal).itself.to.respondTo("ageIsValid");
+        });
+
+        it("Should have method genderIsValid", () => {
+            expect(Animal).itself.to.respondTo("genderIsValid");
+        });
+
+        it("Should have method nameIsValid", () => {
+            expect(Animal).itself.to.respondTo("nameIsValid");
+        });
     });
 
-    it("Should have a valid age", () => {
-        expect(Buddy).to.have.deep.property("age", ANIMAL_TO_CREATE.age);
+    describe("Should not throw error", () => {
+        it("Should create a valid Animal instance", () => {
+            expect(Buddy).to.be.an.instanceOf(Animal);
+        });
+
+        it("Should have a valid age", () => {
+            expect(Buddy).to.have.deep.property("age", ANIMAL_TO_CREATE.age);
+        });
+
+        it("Should have a valid gender", () => {
+            expect(Buddy).to.have.deep.property("gender", ANIMAL_TO_CREATE.gender);
+        });
+
+        it("Should have a valid name", () => {
+            expect(Buddy).to.have.deep.property("name", ANIMAL_TO_CREATE.name);
+        });
     });
 
-    it("Should have a valid gender", () => {
-        expect(Buddy).to.have.deep.property("gender", ANIMAL_TO_CREATE.gender);
-    });
+    describe("Should throw error", () => {
+        it("Should throw error when invalid age is passed", () => {
+            try {
+                Buddy.age = -1;
+            } catch (e) {
+                expect(e.message).to.equal(errorMessages.ageInvalid);
+            }
+        });
 
-    it("Should have a valid name", () => {
-        expect(Buddy).to.have.deep.property("name", ANIMAL_TO_CREATE.name);
-    });
+        it("Should throw error when invalid gender is passed", () => {
+            try {
+                Buddy.gender = "SomethingIncorrect";
+            } catch (e) {
+                expect(e.message).to.equal(errorMessages.genderInvalid);
+            }
+        });
 
-    it("Should throw error when invalid age is passed", () => {
-        try {
-            Buddy.age = -1;
-        } catch (e) {
-            expect(e.message).to.equal(errorMessages.ageInvalid);
-        }
-    });
+        it("Should throw error when invalid name is less than 1 character", () => {
+            try {
+                Buddy.name = "";
+            } catch (e) {
+                expect(e.message).to.equal(errorMessages.nameInvalid);
+            }
+        });
 
-    it("Should throw error when invalid gender is passed", () => {
-        try {
-            Buddy.gender = "SomethingIncorrect";
-        } catch (e) {
-            expect(e.message).to.equal(errorMessages.genderInvalid);
-        }
-    });
-
-    it("Should throw error when invalid name is less than 1 character", () => {
-        try {
-            Buddy.name = "";
-        } catch (e) {
-            expect(e.message).to.equal(errorMessages.nameInvalid);
-        }
-    });
-
-    it("Should throw error when invalid name is greater than 20 characters", () => {
-        try {
-            Buddy.name = "HereIsAReallyLongNameThatShouldNotBeSetToObject";
-        } catch (e) {
-            expect(e.message).to.equal(errorMessages.nameInvalid);
-        }
+        it("Should throw error when invalid name is greater than 20 characters", () => {
+            try {
+                Buddy.name = "HereIsAReallyLongNameThatShouldNotBeSetToObject";
+            } catch (e) {
+                expect(e.message).to.equal(errorMessages.nameInvalid);
+            }
+        });
     });
 });
 
