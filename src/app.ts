b134e6f7834432f7e67452b6e15c2a32807588c0
evan bechtol <evanbechtol@gmail.com>
2019-07-29 07:40:39 -0500

    Doesn't work lol

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {Animal} from "./entities/animal/animal";
+import {Dog} from "./entities/dog/dog";
 
-const Buddy = new Animal(0.5, "male", "Buddy");
-console.log(Buddy.name, Buddy.gender, Buddy.age);
+const Buddy = new Dog(0.5, "male", "Buddy", ["Labrador Retriever", "American Staffordshire Terrier", "Bloodhound"]);
+console.log(Buddy.name, Buddy.gender, Buddy.age, Buddy.sound, Buddy.breeds);
--- src/entities/dog/dog.ts
+++ src/entities/dog/dog.ts
@@ -1,5 +1,26 @@
 import {Animal} from "../animal/animal";
 
-export class Dog extends Animal{
+export class Dog extends Animal {
+    private _breeds: Array<string>;
 
+    /**
+     * @description Constructs a new instance of the dog object
+     * @param age {number} Age of the dog, must be greater than zero
+     * @param gender {string} Gender of the dog, can be either 'male' or 'female'
+     * @param name {string} Name of the dog, must be less than 20 characters
+     * @param breeds {array} Breeds that the dog has
+     */
+    constructor(age: number, gender: string, name: string, breeds: Array<string> = []) {
+        super(age, gender, name, "Woof");
+        this.breeds = breeds as Array<string> ;
+    }
+
+    get breeds() {
+        return this._breeds.join();
+    }
+
+    // Todo: determine if breed should be added to array
+    set breeds(value) {
+        this._breeds = [value];
+    }
 }
