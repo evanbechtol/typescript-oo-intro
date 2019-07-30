3dc65f44365b5d5f1f9c738461b14e3f7ccbdcf6
eevabec <evan.bechtol@ericsson.com>
2019-07-30 14:19:32 -0500

    Fix lint errors for animal/errorMessages.ts

--- src/entities/animal/errorMessages.ts
+++ src/entities/animal/errorMessages.ts
@@ -2,5 +2,5 @@ export = {
     ageInvalid: "Age must be a number greater than zero",
     genderInvalid: "Gender must be a string and must be either 'male' or 'female'",
     nameInvalid: "Name must be a string of length greater than zero, and less than twenty characters",
-    soundInvalid: "Sound must be a string less than fifteen characters"
-}
+    soundInvalid: "Sound must be a string less than fifteen characters",
+};
