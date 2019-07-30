ee0879f81e285f1ad87bf624fcc3816e7f37110f
eevabec <evan.bechtol@ericsson.com>
2019-07-30 14:26:29 -0500

    Fix entities/dog/dog.ts Errors
    - Allow breeds to be set via an array, or passed a string to push to breeds array
    - Create method `isBreedPresent` to prevent duplicate breeds being added
    - Change `_breeds` from `Array<string>` to `string[]`

--- src/entities/dog/dog.ts
+++ src/entities/dog/dog.ts
@@ -1,7 +1,7 @@
 import {Animal} from "../animal/animal";
 
 export class Dog extends Animal {
-    private _breeds: Array<string>;
+    private _breeds: string[];
 
     /**
      * @description Constructs a new instance of the dog object
@@ -10,17 +10,29 @@ export class Dog extends Animal {
      * @param name {string} Name of the dog, must be less than 20 characters
      * @param breeds {array} Breeds that the dog has
      */
-    constructor(age: number, gender: string, name: string, breeds: Array<string> = []) {
+    constructor(age: number, gender: string, name: string, breeds: string[] = []) {
         super(age, gender, name, "Woof");
-        this.breeds = breeds as Array<string> ;
+        this.breeds = breeds;
     }
 
     get breeds() {
-        return this._breeds.join();
+        return this._breeds;
     }
 
-    // Todo: determine if breed should be added to array
-    set breeds(value) {
-        this._breeds = [value];
+    set breeds(value: string[] | string) {
+        if (typeof value === "string" && !this.isBreedPresent(value)) {
+            this._breeds.push(value);
+        } else if (typeof value === "object") {
+            this._breeds = value;
+        }
+    }
+
+    /**
+     * @description Determines if the breed passed is present in the breeds array
+     * @param breed {string} Breed to check for
+     * @returns {boolean} Returns true if the breed is present, false otherwise
+     */
+    private isBreedPresent(breed: string) {
+        return this._breeds.indexOf(breed) !== -1;
     }
 }
