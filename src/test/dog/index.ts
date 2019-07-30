409d0b4a086e9fb43f7559bc5f83a3965d5173a0
eevabec <evan.bechtol@ericsson.com>
2019-07-30 14:30:27 -0500

    Create test/dog/index.ts
    - Create test file for dog entity

---
+++ src/test/dog/index.ts
@@ -0,0 +1,50 @@
+import {expect} from "chai";
+import "mocha";
+import {Dog} from "../../entities/dog/dog";
+
+let Buddy;
+const DOG_TO_CREATE = {
+    age: 0.5,
+    breeds: ["Labrador Retriever", "American Staffordshire Terrier", "Bloodhound"],
+    gender: "male",
+    name: "Buddy",
+};
+
+before(() => {
+    Buddy = dogFactory(DOG_TO_CREATE.age, DOG_TO_CREATE.gender, DOG_TO_CREATE.name, DOG_TO_CREATE.breeds);
+});
+
+describe("Dog Entity", () => {
+
+    describe("Should have methods", () => {
+        it("Should have method isBreedPresent", () => {
+            expect(Buddy).itself.to.respondTo("isBreedPresent");
+        });
+    });
+
+    describe("Should not throw error", () => {
+        it("Should create a valid Dog instance", () => {
+            expect(Buddy).to.be.an.instanceOf(Dog);
+        });
+
+        it("Should have breeds", () => {
+            expect(Buddy).to.have.deep.property("breeds", DOG_TO_CREATE.breeds);
+        });
+
+        it("Should make a sound woof", () => {
+            const expectedSound = "Woof";
+            expect(Buddy.makeSound(1)).to.equal(expectedSound);
+        });
+    });
+});
+
+/**
+ * @description Creates new instance of Dog entity
+ * @param age {number} Age of dog
+ * @param gender {string} Gender of dog
+ * @param name {string} Name of dog
+ * @param breeds {array} Optional: Breeds that dog has
+ */
+function dogFactory(age: number, gender: string, name: string, breeds: string[] = []) {
+    return new Dog(age, gender, name, breeds);
+}
